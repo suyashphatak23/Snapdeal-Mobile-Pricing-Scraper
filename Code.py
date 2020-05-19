0af185a977966022ecfab5f1f1b20b3122d6d5e2
Suyash Phatak <suyashphatakbackup@gmail.com>
2020-05-19 21:55:52 +0530

    Added Code and output image

---
+++ Code.py
@@ -0,0 +1,50 @@
+'''
+Mobile Pricing From Amazon
+@author: Suyash Shivaji Phatak
+Date: 10/05/2020
+'''
+
+# Importing Modules
+from bs4 import BeautifulSoup
+import urllib
+
+# Giving url
+url = "https://www.snapdeal.com/products/mobiles-mobile-phones?sort=plrty&q=Price%3A10000%2C15000%7C"
+
+# Reading all content
+content = urllib.request.urlopen(url).read()
+
+# Passing the content to function
+soup = BeautifulSoup(content, features="lxml")
+
+print('_________________________________________________________________________')
+
+# Printing content
+print('\nAll contents of Websites')
+print(soup.prettify())
+
+# Printing title of website
+print('_________________________________________________________________________')
+print(soup.find('title'))
+print('_________________________________________________________________________')
+
+# Printing all elements of images and their alt text of 250 TV shows
+print('These are Names of Mobile which comes in between Rs.10000 to Rs.15000\n')
+
+# Printing all names of mobile phones using image titles
+element =[]
+for link in soup.find_all('img'):
+    element.append(link.get('title'))
+
+# Removing all None values
+final =[]
+for val in element:
+    if val!= None:
+        final.append(val)
+
+# Printing Final List
+for x in final:
+    print(x)
+    
+print('_________________________________________________________________________')
+
